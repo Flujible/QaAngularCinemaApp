c9dd5a700c8ca00c8baa4c5254e02dd922587998
George Bryant <george.s.bryant@gmail.com>
2019-11-13 15:54:36 +0000

    Add basic sign up and schedule components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ScheduleComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
